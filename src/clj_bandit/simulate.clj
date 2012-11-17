813b7fe7f59e6f9846dcf0aea0a986817c995a38
Paul Ingles <paul@oobaloo.co.uk>
2012-11-17 21:33:11 +0000

    refactoring, on the way to making it easier to test multiple algos with single set of results

--- src/clj_bandit/simulate.clj
+++ src/clj_bandit/simulate.clj
@@ -17,27 +17,35 @@
   (first (keys (:arm pull))))
 
 (defn simulation-results
-  [epsilon iterations simulation-number]
+  [{:keys [algo-name algo variant] :as simulation} iterations simulation-number]
   (let [arm-labels [:arm1 :arm2 :arm3 :arm4 :arm5]
-        arms (map bernoulli-arm [0.1 0.1 0.1 0.1 0.9])
-        algo (epsilon-greedy-algorithm epsilon (atom-storage (mk-arms arm-labels)))]
+        arms (map bernoulli-arm [0.1 0.1 0.1 0.1 0.9])]
     (let [rows (map (fn [t]
                       (let [chosen-arm (arm-name (select-arm algo))
                             reward (draw-arm (nth arms (.indexOf arm-labels chosen-arm)))
                             cumulative-reward 0]
                         (update-reward algo chosen-arm reward)
-                        [epsilon simulation-number t chosen-arm reward]))
+                        [algo-name variant simulation-number t chosen-arm reward]))
                     (range 1 iterations))
           cumulative-rewards (cumulative-sum (map last rows))]
       (map conj rows cumulative-rewards))))
+
+(defn mk-epsilon-algorithm
+  [epsilon]
+  {:algo-name "epsilon-greedy"
+   :variant epsilon
+   :algo (epsilon-greedy-algorithm epsilon (atom-storage (mk-arms [:arm1 :arm2 :arm3 :arm4 :arm5])))})
 
 (defn run-simulation
   ([]
      (run-simulation 1000 200))
   ([simulations iterations]
      (with-open [csv (writer "tmp/results.csv")]
-       (let [epsilon-values [0.1 0.2 0.3 0.4 0.5]]
-         (write-csv csv (apply concat (pmap (fn [epsilon]
-                                              (apply concat (pmap (partial simulation-results epsilon iterations)
-                                                                  (range 1 simulations))))
-                                            epsilon-values)))))))
+       (let [epsilon-algos (map mk-epsilon-algorithm [0.1 0.2 0.3 0.4 0.5])
+             algorithms epsilon-algos
+             results (doall (apply concat (map (fn [{:keys [algo-name variant] :as algorithm}]
+                                                 (println "Testing" algo-name variant)
+                                                 (apply concat (map (partial simulation-results algorithm (inc iterations))
+                                                                    (range 1 (inc simulations)))))
+                                               algorithms)))]
+         (write-csv csv results)))))
