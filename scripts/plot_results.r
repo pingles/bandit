5c358dadd8850f78280deb12db6fd1b18ffca230
Paul Ingles <paul@oobaloo.co.uk>
2012-11-17 03:59:53 +0000

    simulation code works (but is very ugly). add r scripts to capture performance measures

---
+++ scripts/plot_results.r
@@ -0,0 +1,20 @@
+require(plyr)
+require(ggplot2)
+
+setwd("~/Work/clj-bandit/tmp")
+
+results <- read.csv(file="./results.csv", header=F)
+names(results) <- c("epsilon.value", "simulation.number", "t", "chosen.arm", "reward", "cumulative.reward")
+results$epsilon.value <- as.factor(results$epsilon.value)
+
+stats <- ddply(results, c("epsilon.value", "t"), function(df) {mean(df$reward)})
+stats.plot <- ggplot(stats, aes(x = t, y = V1, color = epsilon.value))
+stats.plot + geom_line() + ylim(0, 1) + xlab("Iterations") + ylab("Average Reward") + ggtitle("Epsilon-Greedy Performance")
+
+stats.cumulative.reward <- ddply(results, c("epsilon.value", "t"), function(df) {mean(df$cumulative.reward)})
+stats.cumulative.reward.plot <- ggplot(stats.cumulative.reward, aes(x = t, y = V1, color = epsilon.value))
+stats.cumulative.reward.plot + geom_line() + xlab("Iterations") + ylab("Cumulative Reward") + ggtitle("Epsilon-Greedy Performance")
+
+stats.probability <- ddply(results, c("epsilon.value", "t"), function(df) {mean(df$chosen.arm == ":arm5")})
+stats.probability.plot <- ggplot(stats.probability, aes(x = t, y = V1, color = epsilon.value))
+stats.probability.plot + geom_line() + ylim(c(0, 1)) + xlab("Iterations") + ylab("Pr(Arm=5)") + ggtitle("Epsilon-Greedy Performance")
--- src/clj_bandit/simulate.clj
+++ src/clj_bandit/simulate.clj
@@ -1,6 +1,9 @@
 (ns clj-bandit.simulate
   (:use [clojure.data.csv :only (write-csv)]
-        [clojure.java.io :only (writer)]))
+        [clojure.java.io :only (writer)]
+        [clj-bandit.epsilon]
+        [clj-bandit.storage :only (atom-storage)]
+        [clj-bandit.core :only (select-arm update-reward)]))
 
 (defn bernoulli-arm
   "returns a function mimicking a bandit arm with a set probability. each function call represents the pulling of a bandit arm. can be used to simulate results. 0 < p < 1. p indicates probability of being rewarded (with 1).
@@ -24,17 +27,41 @@
   [pull]
   (first (keys (:arm pull))))
 
-
 (comment
   (let [simulation-seq (repeatedly #(map draw-arm arms))]
     (take 10 simulation-seq)))
+
+(defn cumulative-sum
+  [coll]
+  (reduce 
+   (fn [v, x] (conj v (+ (last v) x))) 
+   [(first coll)] 
+   (rest coll)))
+
+(defn simulation-results
+  [epsilon iterations simulation-number]
+  (let [arm-labels [:arm1 :arm2 :arm3 :arm4 :arm5]
+        arms (map bernoulli-arm [0.1 0.1 0.1 0.1 0.9])
+        algo (epsilon-greedy-algorithm 0.1 (atom-storage arm-labels))]
+    (let [rows (map (fn [t]
+                      (let [chosen-arm (arm-name (select-arm algo))
+                            reward (draw-arm (nth arms (.indexOf arm-labels chosen-arm)))
+                            cumulative-reward 0]
+                        (update-reward algo chosen-arm reward)
+                        [epsilon simulation-number t chosen-arm reward]))
+                    (range 1 iterations))
+          cumulative-rewards (cumulative-sum (map last rows))]
+      (map conj rows cumulative-rewards))))
 
 (defn run-simulation
   "simulations: fixed number of sims to run to cancel out noise within individual tests.
    horizon: number of times algorithm can pull on arms during each simulation. measure how well the algorithm does with 1 try, 100 tries etc."
   ([]
-     (run-simulation 5000 250))
-  ([simulations horizon]
+     (run-simulation 500 200))
+  ([simulations iterations]
      (with-open [csv (writer "tmp/results.csv")]
-       (write-csv csv [["hello" "there"]
-                       ["paul" "test"]]))))
+       (let [epsilon-values [0.1 0.2 0.3 0.4 0.5]]
+         (write-csv csv (apply concat (pmap (fn [epsilon]
+                                              (apply concat (pmap (partial simulation-results epsilon iterations)
+                                                                  (range 1 simulations))))
+                                            epsilon-values)))))))
