d4c55f49fb0aed9760867ce5a43eed8f858d77f7
Paul Ingles <paul@oobaloo.co.uk>
2012-11-19 17:12:48 +0000

    add the plots to the README

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 A Clojure library following ["Bandit Algorithms for Website Optimization"](http://shop.oreilly.com/product/0636920027393.do) by John Myles White.
 
+This library aims to be simple- concerning itself with multi-armed bandit optimisation only. Integrating with web applications etc. is the responsibility of other libraries. Through keeping it small and simple we hope to make it far easier to integrate than existing tools.
+
+## Performance
+
+The ["Bandit Algorithms for Website Optimization"](http://shop.oreilly.com/product/0636920027393.do) uses Monte Carlo Simulation to measure the performance of the algorithms. These can be run using `clj-bandit.simulate/run-simulation`, and `./scripts/plot_results.r` will produce the following plots with ggplot2.
+
+### Average Reward
+
+![Average Reward](http://clojure.bandit.s3-external-3.amazonaws.com/avg_reward.png)
+
+![Accuracy](http://clojure.bandit.s3-external-3.amazonaws.com/accuracy.png)
+
+![Cumulative Reward](http://clojure.bandit.s3-external-3.amazonaws.com/cumulative_reward.png)
+
 ## License
 
 Copyright &copy; Paul Ingles 2012
--- scripts/plot_results.r
+++ scripts/plot_results.r
@@ -4,8 +4,8 @@ require(gridExtra)
 
 setwd("~/Work/clj-bandit/tmp")
 
-#results <- read.csv(file="./results-n1000-t250.csv", header=F)
-results <- read.csv(file="./results.csv", header=F)
+results <- read.csv(file="./results-n1500-t250.csv", header=F)
+#results <- read.csv(file="./results.csv", header=F)
 
 names(results) <- c("algo.name", "algo.variant", "simulation.number", "t", "chosen.arm", "reward", "cumulative.reward")
 results$algo.variant <- as.factor(results$algo.variant)
@@ -17,7 +17,7 @@ facet_wrap(~ algo.name) + geom_line() + xlab("Time (Iteration No.)") + ylab("Ave
 
 stats.probability <- ddply(results, c("algo.name", "algo.variant", "t"), function(df) {mean(df$chosen.arm == ":arm5")})
 stats.probability.plot <- ggplot(stats.probability, aes(x = t, y = V1, color = algo.variant)) +
-facet_wrap(~ algo.name) + geom_line() + ylim(c(0, 1)) + xlab("Time (Iteration No.)") + ylab("Pr(Arm=5)") + ggtitle("Select Best Arm")
+facet_wrap(~ algo.name) + geom_line() + ylim(c(0, 1)) + xlab("Time (Iteration No.)") + ylab("Pr(Arm=5)") + ggtitle("Accuracy")
 
 stats.cumulative.reward <- ddply(results, c("algo.name", "algo.variant", "t"), function(df) {mean(df$cumulative.reward)})
 stats.cumulative.reward.plot <- ggplot(stats.cumulative.reward, aes(x = t, y = V1, color = algo.variant)) +
