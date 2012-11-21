c34e7c7a1b00e39c8cb390a3fff14a0cb3a1cd97
Paul Ingles <paul@oobaloo.co.uk>
2012-11-21 12:44:35 +0000

    change the plots a little

--- scripts/plot_results.r
+++ scripts/plot_results.r
@@ -9,48 +9,35 @@ results <- read.csv(file="./results-n1500-t250.csv", header=F)
 
 names(results) <- c("algo.name", "algo.variant", "algo.parameter", "simulation.number", "t", "chosen.arm", "reward", "cumulative.reward")
 results$algo.variant <- as.factor(results$algo.variant)
+results$algo.parameter <- as.factor(results$algo.parameter)
 results$algo.name <- as.factor(results$algo.name)
 results$t <- as.numeric(results$t)
 
-stats.average.reward <- ddply(results, c("algo.name", "algo.variant", "t"), function(df) {mean(df$reward)})
-stats.average.reward.plot <- ggplot(stats.average.reward, aes(x = t, y = V1, color = algo.variant)) +
-  facet_wrap(~ algo.name) + geom_line() + xlab("Time (Iteration No.)") + ylab("Average Reward") + ggtitle("Average Reward") + ylim(c(0, 1))
+stats.average.reward <- ddply(results, c("algo.name", "algo.variant", "algo.parameter", "t"), function(df) {mean(df$reward)})
+stats.average.reward.plot <- ggplot(stats.average.reward, aes(x = t, y = V1, color = algo.parameter)) +
+  facet_wrap(~ algo.name + algo.variant) + geom_line() + xlab("Time (Iteration No.)") + ylab("Average Reward") + ggtitle("Average Reward") + ylim(c(0, 1))
 
-stats.probability <- ddply(results, c("algo.name", "algo.variant", "t"), function(df) {mean(df$chosen.arm == ":arm5")})
-stats.probability.plot <- ggplot(stats.probability, aes(x = t, y = V1, color = algo.variant)) +
-  facet_wrap(~ algo.name) + geom_line() + ylim(c(0, 1)) + xlab("Time (Iteration No.)") + ylab("Pr(Arm=5)") + ggtitle("Accuracy")
+stats.average.reward.plot
 
-stats.cumulative.reward <- ddply(results, c("algo.name", "algo.variant", "t"), function(df) {mean(df$cumulative.reward)})
-stats.cumulative.reward.plot <- ggplot(stats.cumulative.reward, aes(x = t, y = V1, color = algo.variant)) +
-  facet_wrap(~ algo.name) + geom_line() + xlab("Time (Iteration No.)") + ylab("Cumulative Reward") + ggtitle("Cumulative Reward Performance")
+stats.probability <- ddply(results, c("algo.name", "algo.variant", "algo.parameter", "t"), function(df) {mean(df$chosen.arm == ":arm5")})
+stats.probability.plot <- ggplot(stats.probability, aes(x = t, y = V1, color = algo.parameter)) +
+  facet_wrap(~ algo.name + algo.variant) + geom_line() + ylim(c(0, 1)) + xlab("Time (Iteration No.)") + ylab("Pr(Arm=5)") + ggtitle("Accuracy")
 
-# lets plot the best algorithms on a single cumulative plot
-stats.cumulative.reward$algo.name <- as.factor(stats.cumulative.reward$algo.name)
-stats.cumulative.reward$algo.variant <- as.factor(stats.cumulative.reward$algo.variant)
-algoName <- function(name, variant) {
-  paste(name, variant)
-}
-stats.cumulative.reward.maxes <- ddply(stats.cumulative.reward, c("algo.name", "algo.variant"), function(df) {max(df$V1)})
-stats.cumulative.reward.maxes$algo.label <- algoName(stats.cumulative.reward.maxes$algo.name, stats.cumulative.reward.maxes$algo.variant)
-stats.cumulative.reward.maxes.plot <- ggplot(stats.cumulative.reward.maxes, aes(x=algo.label, y=V1, fill=algo.name)) +
-  geom_bar() + ylab("Cumulative Reward") + xlab("Algorithm Parameter") + ggtitle("Maximum Reward") + scale_x_discrete(labels=stats.cumulative.reward.maxes$algo.variant)
-stats.cumulative.reward.maxes.plot
+stats.probability.plot
+
+stats.cumulative.reward <- ddply(results, c("algo.name", "algo.variant", "algo.parameter", "t"), function(df) {mean(df$cumulative.reward)})
+stats.cumulative.reward.plot <- ggplot(stats.cumulative.reward, aes(x = t, y = V1, color = algo.parameter)) +
+  facet_wrap(~ algo.name + algo.variant) + geom_line() + xlab("Time (Iteration No.)") + ylab("Cumulative Reward") + ggtitle("Cumulative Reward Performance")
+
+stats.cumulative.reward.plot
 
 # This should use the numbers from iteration/horizon 250. That is, the results of all simulations
 # at that time.
-stats.cumulative.final <- subset(results, t == 250, select=c("algo.name", "algo.variant", "cumulative.reward"))
-stats.cumulative.final$algo.label <- algoName(stats.cumulative.final$algo.name, stats.cumulative.final$algo.variant)
+stats.cumulative.final <- subset(results, t == 250, select=c("algo.name", "algo.variant", "algo.parameter", "cumulative.reward"))
+stats.cumulative.final.boxplot <- ggplot(stats.cumulative.final, aes(paste(algo.variant, algo.parameter), cumulative.reward)) +
+  facet_wrap(~ algo.name) + geom_boxplot() + xlab("Algorithm") + ylab("Reward") + ggtitle("Algorithm Reward at t=250") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-stats.cumulative.final.boxplot <- ggplot(stats.cumulative.final, aes(algo.label, cumulative.reward)) +
-  geom_boxplot() + xlab("Algorithm") + ylab("Reward") + ggtitle("Algorithm Reward at t=250") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 stats.cumulative.final.boxplot
-
-grid.arrange(stats.cumulative.reward.maxes.plot, stats.cumulative.reward.boxplot)
-
-# view individually
-stats.average.reward.plot
-stats.probability.plot
-stats.cumulative.reward.plot
 
 # or as a grid
 grid.arrange(stats.average.reward.plot, stats.probability.plot, stats.cumulative.reward.plot)
