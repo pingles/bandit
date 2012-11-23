e49d2f4af207133ba71a843b09406e2b5227a319
Paul Ingles <paul@oobaloo.co.uk>
2012-11-23 14:26:25 +0000

    start working on a defrecord for an Arm- trying new model in epsilon greedy, can update rest later

--- src/clj_bandit/algo/epsilon.clj
+++ src/clj_bandit/algo/epsilon.clj
@@ -1,16 +1,56 @@
 (ns clj-bandit.algo.epsilon
-  (:use [clj-bandit.core :only (BanditAlgorithm best-performing update-arms total-pulls)]
+  (:use [clj-bandit.core :only (BanditAlgorithm update-arms)]
         [clj-bandit.storage :only (get-arms put-arms)]))
 
+(defrecord Arm [name n value])
+
+(defn mk-arm
+  ([name]
+     (Arm. name 0 0))
+  ([name & keyvals]
+     (apply assoc (mk-arm name) keyvals)))
+
+(defn total-pulls
+  [arms]
+  (reduce + (map :n arms)))
+
+(defn best-performing
+  [arms]
+  (apply max-key :value arms))
+
 (defn draw-arm
-  [epsilon arms]
-  (if (> (rand) epsilon)
-    (best-performing :value arms)
-    (apply hash-map (rand-nth (seq arms)))))
+  ([epsilon arms]
+     (draw-arm (rand) arms))
+  ([epsilon n arms]
+     (if (> n epsilon)
+       (best-performing arms)
+       (rand-nth (seq arms)))))
 
 (defn draw-anneal-arm
   [anneal arms]
   (draw-arm (anneal (total-pulls arms)) arms))
+
+
+(defn weighted-value
+  [n value reward]
+  (+ (* (/ (dec n) n)
+        value)
+     (* (/ 1 n)
+        reward)))
+
+(defn reward
+  "updates the arm with its new weighted value."
+  [{:keys [n value] :as arm} reward]
+  (let [u (assoc arm :n (inc (:n arm)))]
+    (if (zero? n)
+      (assoc u :value reward)
+      (assoc u :value (weighted-value n value reward)))))
+
+
+
+
+
+
 
 (defmulti epsilon-greedy-algorithm
   "epsilon can either be a constant factor, or a function that will be
--- test/clj_bandit/algo/epsilon_test.clj
+++ test/clj_bandit/algo/epsilon_test.clj
@@ -2,7 +2,8 @@
   (:use [clojure.test]
         [clj-bandit.core :only (mk-arms arms update-reward)]
         [clj-bandit.storage :only (atom-storage)]
-        [clj-bandit.algo.epsilon] :reload))
+        [clj-bandit.algo.epsilon] :reload)
+  (:require [clj-bandit.algo.epsilon :as e]))
 
 (deftest epsilon-greedy-bandit
   (let [algo (epsilon-greedy-algorithm 0.2 (atom-storage (mk-arms #{:lever1 :lever2})))]
@@ -13,3 +14,31 @@
     (is (= {:lever1 {:reward 1 :n 1 :value 1}
             :lever2 {:reward 0 :n 0 :value 0}}
            (arms algo)))))
+
+(deftest best-performing-arm
+  (is (= (e/mk-arm :arm2 :value 100)
+         (best-performing [(e/mk-arm :arm1 :value 1)
+                           (e/mk-arm :arm2 :value 100)]))))
+
+(deftest calculating-total-pulls
+  (is (= 101
+         (total-pulls [(e/mk-arm :arm1 :n 1)
+                       (e/mk-arm :arm2 :n 100)]))))
+
+(deftest drawing-arms
+  (testing "standard"
+    (is (= (e/mk-arm :arm2 :value 2)
+           (draw-arm 0.1 0.2 [(e/mk-arm :arm1 :value 1)
+                              (e/mk-arm :arm2 :value 2)])))))
+
+(deftest tracking-arm-reward
+  (let [arm (e/mk-arm :arm1 :value 0 :n 0)]
+    (is (= (e/mk-arm :arm1 :n 1 :value 0)
+           (reward arm 0)))
+    (is (= (e/mk-arm :arm1 :n 1 :value 1)
+           (reward arm 1)))
+    (is (= (e/mk-arm :arm1 :n 3 :value 1/2)
+           (-> arm
+               (reward 0)
+               (reward 0)
+               (reward 1))))))
