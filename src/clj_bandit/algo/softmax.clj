68c1b63c4fc84adc1944db678b820533087492e2
Paul Ingles <paul@oobaloo.co.uk>
2012-11-24 13:04:24 +0000

    update softmax to use new arm data structure

--- src/clj_bandit/algo/softmax.clj
+++ src/clj_bandit/algo/softmax.clj
@@ -1,7 +1,5 @@
 (ns clj-bandit.algo.softmax
-  (:use [clj-bandit.core :only (cumulative-sum BanditAlgorithm update-arms anneal total-pulls)]
-        [clj-bandit.storage :only (get-arms put-arms)]
-        [clojure.math.numeric-tower :only (sqrt expt)]))
+  (:use [clj-bandit.core :only (cumulative-sum)]))
 
 (defn z
   [temperature values]
@@ -13,76 +11,27 @@
   (/ (Math/exp (/ value temperature))
      z))
 
+(defn probability
+  [temperature {:keys [value] :as arm} arms]
+  (let [values (map :value arms)
+        zval (z temperature values)]
+    (p temperature zval value)))
+
 (defn probabilities
-  [temperature values]
-  (let [z-value (z temperature values)]
-    (map (partial p temperature z-value) values)))
-
-(defn probability-maps
-  [temperature values]
-  (let [probs (probabilities temperature values)
-        cum-probs (cumulative-sum probs)]
-    (map (fn [[p cum-p]] {:p p :cum-p cum-p})
-         (partition 2 (interleave probs cum-probs)))))
-
-(defn select-cumulative-probability
-  [val arms]
-  (if-let [found (first (filter (fn [[_ {:keys [cum-p]}]]
-                                  (> cum-p val)) arms))]
-    (apply hash-map found)))
+  "adds softmax p values to arms"
+  [temperature arms]
+  (let [probs (map #(probability temperature % arms) arms)
+        arms (map (fn [arm p] (assoc arm :p p)) arms probs)]
+    (map #(assoc %1 :cumulative-p %2)
+         arms
+         (cumulative-sum probs))))
 
 (defn select-draw
-  "Given arms with cum-p values, pick the one we'll use"
-  [rand-val arms]
-  (or (select-cumulative-probability rand-val arms)
-      (apply hash-map (last arms))))
-
-(defn draw-probabilities
-  [temperature arms]
-  (let [arm-vec (vec arms)
-        values (map #(:value (last %)) arm-vec)]
-    (let [p-maps (probability-maps temperature values)
-          arm-labels (map first arm-vec)
-          arm-vals (map last arm-vec)
-          merged-arm-vals (map #(apply merge %)
-                               (partition 2 (interleave arm-vals p-maps)))]
-      (apply merge (map #(apply hash-map %)
-                        (partition 2 (interleave arm-labels merged-arm-vals)))))))
-
-(defn make-draw
   ([temperature arms]
-     (make-draw temperature (rand) arms))
+     (select-draw (rand) arms))
   ([temperature rand-val arms]
-     (if (not-every? zero? (map :value (vals arms)))
-       (select-draw rand-val (draw-probabilities temperature arms))
-       (apply hash-map (last arms)))))
-
-(defn draw-arm
-  [temperature arms]
-  (make-draw temperature arms))
-
-(defn draw-anneal-arm
-  [annealfn arms]
-  (draw-arm (anneal (total-pulls arms)) arms))
-
-(defmulti softmax-algorithm
-  "temperature controls how stable the algorithm is. The lower the temperature the more stable. 0 < temperature < 1"
-  (fn [temperature _] (number? temperature)))
-
-(defn- mk-algorithm
-  [storage selectfn]
-  (reify BanditAlgorithm
-    (select-arm [_]
-      (selectfn (get-arms storage)))
-    (update-reward [_ arm reward]
-      (put-arms storage #(update-arms reward arm %)))
-    (arms [_]
-      (get-arms storage))))
-
-(defmethod softmax-algorithm true
-  [temperature storage]
-  (mk-algorithm storage (partial draw-arm temperature)))
-
-(defmethod softmax-algorithm false
-  [annealfn storage]
-  (mk-algorithm storage (partial draw-anneal-arm annealfn)))
+     (or (first (filter #(zero? (:pulls %)) arms))
+         (first (filter (fn [{:keys [cumulative-p]}]
+                          (> cumulative-p rand-val))
+                        (probabilities temperature arms)))
+         (last arms))))
--- test/clj_bandit/algo/softmax_test.clj
+++ test/clj_bandit/algo/softmax_test.clj
@@ -1,65 +1,89 @@
 (ns clj-bandit.algo.softmax_test
   (use [clojure.test]
-       [clj-bandit.core :only (select-arm cumulative-sum)]
-       [clj-bandit.storage :only (atom-storage)]
+       [clj-bandit.core :only (cumulative-sum)]
+       [clj-bandit.arms :only (mk-arm)]
        [clj-bandit.algo.softmax] :reload))
 
 (deftest z-values
   (is (= 1.0687444933941748E13
          (z 1/10 [2 2 3 1])))
   (is (= 49.52498537632265
-         (z 9/10 [2 2 3 1])))
-  (is (= [2.061060046209062E-9 4.539786860886666E-5 0.9999546000703311]
-         (probabilities 1/10 [1 2 3])))
-  (is (= [0.07538325148667332 0.22899409853365954 0.6956226499796672]
-         (probabilities 9/10 [1 2 3])))
-  (is (= [2.061060046209062E-9 4.5399929668912874E-5 1.0]
-         (cumulative-sum (probabilities 1/10 [1 2 3]))))
-  (is (= [0.015876239976466765 0.1331866678026651 0.9999999999999999]
-         (cumulative-sum (probabilities 5/10 [1 2 3])))))
+         (z 9/10 [2 2 3 1]))))
 
-(deftest picking
-  (is (= {:arm2 {:cum-p 1.9287498479639178E-22
-                 :p 1.9287498479639178E-22
-                 :reward 0
-                 :n 11
-                 :value 10}
-          :arm3 {:cum-p 1.9287498479639178E-22
-                 :p 3.7200759760208356E-44
-                 :reward 0
-                 :n 11
-                 :value 5}
-          :arm1 {:cum-p 1.0
-                 :p 1.0
-                 :reward 0
-                 :n 12
-                 :value 15}}
-         (draw-probabilities 0.1 {:arm1 {:n 12 :reward 0 :value 15}
-                                  :arm2 {:n 11 :reward 0 :value 10}
-                                  :arm3 {:n 11 :reward 0 :value 5}})))
-  (is (= {:arm1 {:cum-p 1.0
-                 :p 1.0
-                 :reward 0
-                 :n 12
-                 :value 15}}
-         (make-draw 0.1 0.3 {:arm1 {:n 12 :reward 0 :value 15}
-                             :arm2 {:n 11 :reward 0 :value 10}
-                             :arm3 {:n 11 :reward 0 :value 5}})))
-  (is (= {:arm1 {:n 0
-                 :reward 0
-                 :value 0}}
-         (make-draw 0.1 0.3 {:arm1 {:n 0 :reward 0 :value 0}
-                             :arm2 {:n 0 :reward 0 :value 0}
-                             :arm3 {:n 0 :reward 0 :value 0}})))
-  (is (= {:arm3 {:cum-p 1}}
-         (select-draw 0.92 {:arm1 {:cum-p 0.28}
-                            :arm2 {:cum-p 0.39}
-                            :arm3 {:cum-p 1}})))
-  (is (= {:arm2 {:cum-p 0.39}}
-         (select-draw 0.3 {:arm1 {:cum-p 0.28}
-                           :arm2 {:cum-p 0.39}
-                           :arm3 {:cum-p 1}})))
-  (is (= {:arm1 {:cum-p 0.28}}
-         (select-draw 1.2 {:arm1 {:cum-p 0.28}
-                           :arm2 {:cum-p 0.39}
-                           :arm3 {:cum-p 1}}))))
+(deftest adding-probabilities
+  (is (= 0.5
+         (probability 1
+                      (mk-arm :arm1)
+                      [(mk-arm :arm1)
+                       (mk-arm :arm2)])))
+  (let [probs (probabilities 1
+                             [(mk-arm :arm1 :value 1)
+                              (mk-arm :arm2 :value 10)])]
+    (is (= :arm1
+           (->> probs first :name)))
+    (is (= 1.2339457598623172E-4
+           (->> probs first :p)))
+    (is (= [1.2339457598623172E-4 0.9999999999999999]
+           (map :cumulative-p probs)))))
+
+(deftest selecting-arms
+  (testing "before every arm"
+    (is (= (mk-arm :arm1)
+           (select-draw 1 1 [(mk-arm :arm1)
+                             (mk-arm :arm2)])))
+    (is (= (mk-arm :arm2)
+           (select-draw 1 1 [(mk-arm :arm1 :pulls 1)
+                             (mk-arm :arm2)]))))
+  (testing "after every arm has been pulled"
+    (let [arms [(mk-arm :arm1 :cumulative-p 0.1 :pulls 10)
+                (mk-arm :arm2 :cumulative-p 0.9 :pulls 10)]]
+      (is (= (mk-arm :arm1 :p 0.5 :cumulative-p 0.5 :pulls 10)
+             (select-draw 1 0.05 arms)))
+      (is (= (mk-arm :arm2 :p 0.5 :cumulative-p 1.0 :pulls 10)
+             (select-draw 1 0.91 arms))))))
+
+;; (deftest picking
+;;   (is (= {:arm2 {:cum-p 1.9287498479639178E-22
+;;                  :p 1.9287498479639178E-22
+;;                  :reward 0
+;;                  :n 11
+;;                  :value 10}
+;;           :arm3 {:cum-p 1.9287498479639178E-22
+;;                  :p 3.7200759760208356E-44
+;;                  :reward 0
+;;                  :n 11
+;;                  :value 5}
+;;           :arm1 {:cum-p 1.0
+;;                  :p 1.0
+;;                  :reward 0
+;;                  :n 12
+;;                  :value 15}}
+;;          (draw-probabilities 0.1 {:arm1 {:n 12 :reward 0 :value 15}
+;;                                   :arm2 {:n 11 :reward 0 :value 10}
+;;                                   :arm3 {:n 11 :reward 0 :value 5}})))
+;;   (is (= {:arm1 {:cum-p 1.0
+;;                  :p 1.0
+;;                  :reward 0
+;;                  :n 12
+;;                  :value 15}}
+;;          (make-draw 0.1 0.3 {:arm1 {:n 12 :reward 0 :value 15}
+;;                              :arm2 {:n 11 :reward 0 :value 10}
+;;                              :arm3 {:n 11 :reward 0 :value 5}})))
+;;   (is (= {:arm1 {:n 0
+;;                  :reward 0
+;;                  :value 0}}
+;;          (make-draw 0.1 0.3 {:arm1 {:n 0 :reward 0 :value 0}
+;;                              :arm2 {:n 0 :reward 0 :value 0}
+;;                              :arm3 {:n 0 :reward 0 :value 0}})))
+;;   (is (= {:arm3 {:cum-p 1}}
+;;          (select-draw 0.92 {:arm1 {:cum-p 0.28}
+;;                             :arm2 {:cum-p 0.39}
+;;                             :arm3 {:cum-p 1}})))
+;;   (is (= {:arm2 {:cum-p 0.39}}
+;;          (select-draw 0.3 {:arm1 {:cum-p 0.28}
+;;                            :arm2 {:cum-p 0.39}
+;;                            :arm3 {:cum-p 1}})))
+;;   (is (= {:arm1 {:cum-p 0.28}}
+;;          (select-draw 1.2 {:arm1 {:cum-p 0.28}
+;;                            :arm2 {:cum-p 0.39}
+;;                            :arm3 {:cum-p 1}}))))
