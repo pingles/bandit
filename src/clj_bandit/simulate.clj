f98660ccdd221c5e4432fd1637bd7d16c82d440e
Paul Ingles <paul@oobaloo.co.uk>
2012-11-24 17:40:33 +0000

    start of new simulator- use iterate to move through the sequence

--- src/clj_bandit/simulate.clj
+++ src/clj_bandit/simulate.clj
@@ -1,7 +1,9 @@
 (ns clj-bandit.simulate
   (:import [java.util UUID])
   (:use [clojure.string :only (join)]
-        [clojure.java.io :only (writer)]))
+        [clojure.java.io :only (writer)]
+        [clj-bandit.arms :only (mk-arm fold-arm reward)])
+  (:require [clj-bandit.algo.epsilon :as e]))
 
 (set! *warn-on-reflection* true)
 
@@ -10,11 +12,35 @@
 (defn draw-arm [f] (f))
 
 (defn mk-bernoulli-bandit
-  "creates the simulation bandit: a vector of arms that reward with fixed probability."
+  "creates the simulation bandit: a map of labels to their arms (functions)"
   [& p]
-  (map bernoulli-arm p))
+  (reduce merge
+          (map (fn [[label pct]] {label (bernoulli-arm pct)})
+               (partition 2 p))))
 
 (defn simulate
   "runs a simulation. bandit is a sequence of arms (functions) that
-   return their numerical reward value."
-  [bandit])
+   return their numerical reward value.
+   arms: the initial arms map"
+  [bandit epsilon arms]
+  (let [pull (e/select-arm epsilon arms)
+        selected-label (:name pull)
+        arm (get bandit selected-label)
+        rwd (draw-arm arm)]
+    (println "Pulling" selected-label "for reward" rwd)
+    (fold-arm (reward pull rwd) arms)))
+
+(defn simulation-seq
+  [bandit epsilon arms]
+  (iterate (partial simulate bandit epsilon) arms))
+
+(comment
+  (def bandit (mk-bernoulli-bandit :arm1 0.1
+                                   :arm2 0.1
+                                   :arm3 0.1
+                                   :arm4 0.1
+                                   :arm5 0.9))
+  (def epsilon 0.1)
+  (def arms (map mk-arm [:arm1 :arm2 :arm3 :arm4 :arm5]))
+  (simulate bandit epsilon arms)
+  (def some-results (take 5 (simulation-seq bandit epsilon arms))))
