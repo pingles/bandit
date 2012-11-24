c43051be8a7b5ffa5793782ba83551ccadf78720
Paul Ingles <paul@oobaloo.co.uk>
2012-11-24 12:23:14 +0000

    deleted comments

--- src/clj_bandit/algo/epsilon.clj
+++ src/clj_bandit/algo/epsilon.clj
@@ -27,36 +27,3 @@
   (select-arm 0.1 (fold-arm (reward selected-arm 1) arms)))
 
 
-
-
-
-;; TODO
-;; Want to delete all the stuff below. just need a way to fold
-;; updated arms together
-
-
-;; (defmulti epsilon-greedy-algorithm
-;;   "epsilon can either be a constant factor, or a function that will be
-;;    applied to the current number of pulls. use with anneal to cause
-;;    algorithm to favour exploitation over time."
-;;   (fn [x _] (number? x)))
-
-;; (defn- mk-algorithm
-;;   [storage selectfn]
-;;   (reify BanditAlgorithm
-;;     (select-arm [_]
-;;       (selectfn (get-arms storage)))
-;;     (update-reward [_ arm reward]
-;;       (put-arms storage #(update-arms reward arm %)))
-;;     (arms [_]
-;;       (get-arms storage))))
-
-;; (defmethod epsilon-greedy-algorithm true
-;;   [epsilon storage]
-;;   (mk-algorithm storage (partial draw-arm epsilon)))
-
-;; (defmethod epsilon-greedy-algorithm false
-;;   [annealfn storage]
-;;   (mk-algorithm storage (partial draw-anneal-arm annealfn)))
-
-
