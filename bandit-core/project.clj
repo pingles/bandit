7dd10862991b66c6c09cd29d220fa28562f89b14
Paul Ingles <paul@oobaloo.co.uk>
2013-05-30 21:27:35 +0100

    remove dev dependencies from core- now part of simulate; add core dependency to simulate module

--- bandit-core/project.clj
+++ bandit-core/project.clj
@@ -4,6 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]]
-  :profiles {:dev {:dependencies [[criterium "0.3.0"]
-                                  [org.clojure/data.csv "0.1.2"]]}})
+                 [org.clojure/math.numeric-tower "0.0.1"]])
