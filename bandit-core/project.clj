28eb5046fd42472a91d5b7853de917a919c81979
Paul Ingles <paul@oobaloo.co.uk>
2013-06-04 20:59:01 +0100

    update to latest releases

--- bandit-core/project.clj
+++ bandit-core/project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/pingles/clj-bandit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
--- project.clj
+++ project.clj
@@ -5,7 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-bandit/bandit-core "0.2.0-SNAPSHOT"]
                  [clj-bandit/bandit-core "0.2.0-SNAPSHOT"]]
-	:plugins [[lein-sub "0.2.4"]]
-	:sub ["bandit-core" "bandit-simulate"]
-  :profiles {:dev {:dependencies [[criterium "0.3.0"]
-                                  [org.clojure/data.csv "0.1.2"]]}})
+  :plugins [[lein-sub "0.2.4"]]
+  :sub ["bandit-core" "bandit-simulate"])
