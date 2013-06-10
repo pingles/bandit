c4e7725a8d268cba1c9b998608462a77156867ee
Paul Ingles <paul@oobaloo.co.uk>
2013-06-10 14:52:19 +0100

    convert epsilon algo to expectations

--- bandit-core/test/clj_bandit/algo/epsilon_test.clj
+++ bandit-core/test/clj_bandit/algo/epsilon_test.clj
@@ -1,34 +1,23 @@
 (ns clj-bandit.algo.epsilon-test
-  (:use [clojure.test]
+  (:use [expectations]
         [clj-bandit.arms :only (mk-arm best-performing total-pulls reward fold-arm)]
-        [clj-bandit.algo.epsilon] :reload))
+        [clj-bandit.algo.epsilon]))
 
-(deftest best-performing-arm
-  (is (= (mk-arm :arm2 :value 100)
-         (best-performing :value [(mk-arm :arm1 :value 1)
-                                  (mk-arm :arm2 :value 100)]))))
+;; select the best performing arm
+(given (best-performing :value [(mk-arm :arm1) (mk-arm :arm2 :value 1)])
+       (expect :name :arm2
+               :value 1))
 
-(deftest calculating-total-pulls
-  (is (= 101
-         (total-pulls [(mk-arm :arm1 :pulls 1)
-                       (mk-arm :arm2 :pulls 100)]))))
+(expect 1 (total-pulls [(mk-arm :arm1 :pulls 1) (mk-arm :arm2)]))
 
-(deftest tracking-arm-reward
-  (let [arm (mk-arm :arm1 :value 0 :pulls 0)]
-    (is (= (mk-arm :arm1 :pulls 1 :value 0)
-           (reward arm 0)))
-    (is (= (mk-arm :arm1 :pulls 1 :value 1)
-           (reward arm 1)))
-    (is (= (mk-arm :arm1 :pulls 3 :value 1/2)
-           (-> arm
-               (reward 0)
-               (reward 0)
-               (reward 1))))))
+;; tracking arm reward
+(given (-> (mk-arm :arm1) (reward 0) (reward 1))
+       (expect :name :arm1
+               :value 1
+               :pulls 2))
 
-(deftest updating-bandit-state
-  (let [arms [(mk-arm :arm1)
-              (mk-arm :arm2)]]
-    (is (= [(mk-arm :arm1 :pulls 1)
-            (mk-arm :arm2 :pulls 0)]
-           (fold-arm (mk-arm :arm1 :pulls 1)
-                     arms)))))
+;; folding arm results
+(expect [(mk-arm :arm1 :pulls 1) (mk-arm :arm2)]
+        (fold-arm (mk-arm :arm1 :pulls 1)
+                  [(mk-arm :arm1) (mk-arm :arm2)]))
+
