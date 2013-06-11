db2a54aa559d1a9f6254ae2ce2f1eed7c050dec4
Paul Ingles <paul@oobaloo.co.uk>
2013-06-11 08:49:19 +0100

    convert softmax tests to use expectations

--- bandit-core/test/clj_bandit/algo/softmax_test.clj
+++ bandit-core/test/clj_bandit/algo/softmax_test.clj
@@ -1,44 +1,25 @@
 (ns clj-bandit.algo.softmax_test
-  (use [clojure.test]
+  (use [expectations]
        [clj-bandit.arms :only (mk-arm)]
-       [clj-bandit.algo.softmax] :reload))
+       [clj-bandit.algo.softmax]))
 
-(deftest z-values
-  (is (= 1.0687444933941748E13
-         (z 1/10 [2 2 3 1])))
-  (is (= 49.52498537632265
-         (z 9/10 [2 2 3 1]))))
+;; z-values
+(expect 1.0687444933941748E13 (z 1/10 [2 2 3 1]))
+(expect 49.52498537632265 (z 9/10 [2 2 3 1]))
 
-(deftest adding-probabilities
-  (is (= 0.5
-         (probability 1
-                      (mk-arm :arm1)
-                      [(mk-arm :arm1)
-                       (mk-arm :arm2)])))
-  (let [probs (probabilities 1
-                             [(mk-arm :arm1 :value 1)
-                              (mk-arm :arm2 :value 10)])]
-    (is (= :arm1
-           (->> probs first :name)))
-    (is (= 1.2339457598623172E-4
-           (->> probs first :p)))
-    (is (= [1.2339457598623172E-4 0.9999999999999999]
-           (map :cumulative-p probs)))))
+;; probabilities
+(expect 0.5 (probability 1 (mk-arm :arm1) [(mk-arm :arm1) (mk-arm :arm2)]))
+(given (probabilities 1 [(mk-arm :arm1 :value 1) (mk-arm :arm2 :value 10)])
+       (expect #(map :cumulative-p %) [1.2339457598623172E-4 0.9999999999999999]
+               #(map :p %) [1.2339457598623172E-4 0.9998766054240137]))
 
-(deftest selecting-arms
-  (testing "before every arm"
-    (is (= (mk-arm :arm1)
-           (select-arm 1 1 [(mk-arm :arm1)
-                             (mk-arm :arm2)])))
-    (is (= (mk-arm :arm2)
-           (select-arm 1 1 [(mk-arm :arm1 :pulls 1)
-                             (mk-arm :arm2)]))))
-  (testing "after every arm has been pulled"
-    (let [arms [(mk-arm :arm1 :cumulative-p 0.1 :pulls 10)
-                (mk-arm :arm2 :cumulative-p 0.9 :pulls 10)]]
-      (is (= (mk-arm :arm1 :p 0.5 :cumulative-p 0.5 :pulls 10)
-             (select-arm 1 0.05 arms)))
-      (is (= (mk-arm :arm2 :p 0.5 :cumulative-p 1.0 :pulls 10)
-             (select-arm 1 0.91 arms)))
-      (is (= (mk-arm :arm2 :cumulative-p 0.9 :pulls 10)
-             (select-arm 1 100 arms))))))
+;; select unpulled arms
+(expect (mk-arm :arm1) (select-arm 1 1 [(mk-arm :arm1) (mk-arm :arm2)]))
+(expect (mk-arm :arm2) (select-arm 1 1 [(mk-arm :arm1 :pulls 1) (mk-arm :arm2)]))
+
+;; selecting arms by their cumulative probability
+(let [arms [(mk-arm :arm1 :cumulative-p 0.1 :pulls 10)
+            (mk-arm :arm2 :cumulative-p 0.9 :pulls 10)]]
+  (expect (mk-arm :arm1 :p 0.5 :cumulative-p 0.5 :pulls 10) (select-arm 1 0.05 arms))
+  (expect (mk-arm :arm2 :p 0.5 :cumulative-p 1.0 :pulls 10) (select-arm 1 0.91 arms))
+  (expect (mk-arm :arm2 :cumulative-p 0.9 :pulls 10) (select-arm 1 100 arms)))
