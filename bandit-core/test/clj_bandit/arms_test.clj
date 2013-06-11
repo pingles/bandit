ef82a5a567a88e06b0c616907809f9e620d5d574
Paul Ingles <paul@oobaloo.co.uk>
2013-06-11 09:18:27 +0100

    add some expectations around the arms ns

--- bandit-core/test/clj_bandit/arms_test.clj
+++ bandit-core/test/clj_bandit/arms_test.clj
@@ -2,8 +2,23 @@
   (:use [expectations]
         [clj-bandit.arms]))
 
+;; finding unpulled arms
+(expect empty? (unpulled [(mk-arm :arm1 :pulls 1)]))
+(expect [(mk-arm :arm1)] (unpulled [(mk-arm :arm1) (mk-arm :arm2 :pulls 1)]))
+
+;; finding best value
+(expect (mk-arm :arm1 :value 10) (best-performing :value [(mk-arm :arm1 :value 10)
+                                                          (mk-arm :arm2)]))
+
+;; total pulls
+(expect 10 (total-pulls [(mk-arm :arm1 :pulls 5) (mk-arm :arm2 :pulls 5)]))
+
+;; weighted values
+(expect 1 (weighted-value (mk-arm :arm1 :pulls 1 :value 0) 1))
+(expect 1 (weighted-value (mk-arm :arm1 :pulls 2 :value 1) 1))
+(expect 2/3 (weighted-value (mk-arm :arm1 :pulls 3 :value 1) 0))
+
 ;; folding arm results
 (expect [(mk-arm :arm1 :pulls 1) (mk-arm :arm2)]
         (fold-arm (mk-arm :arm1 :pulls 1)
                   [(mk-arm :arm1) (mk-arm :arm2)]))
-
