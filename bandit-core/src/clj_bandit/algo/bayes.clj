76160fb2a1bfc9afcbfdc27726491b718584d526
Paul Ingles <paul@oobaloo.co.uk>
2013-12-16 21:55:56 +0000

    refactor bayes algo a little; add some tests to show how alpha and beta change during test

--- bandit-core/src/clj_bandit/algo/bayes.clj
+++ bandit-core/src/clj_bandit/algo/bayes.clj
@@ -8,17 +8,22 @@
 ;; implemented here: http://www.chrisstucchio.com/blog/2013/bayesian_bandit.html
 ;; by Chris Stucchio.
 
+(defn estimate-value
+  [{:keys [pulls value prior] :as arm}]
+  (let [pr (or prior 1.0)
+        alpha (+ pr value)
+        beta  (+ pr (- pulls value))
+        dist  (beta-distribution alpha beta)]
+    (assoc arm
+      :theta (draw dist)
+      :alpha alpha
+      :beta beta)))
+
 (defn select-arm
   "Returns the arm that should be pulled. Arm records should have
    a :prior value (assumed to be 1.0 if no prior set)."
   [arms]
-  (let [assoc-theta (fn [{:keys [pulls value prior] :as arm}]
-                      (let [pr (or prior 1.0)
-                            alpha (+ pr value)
-                            beta  (+ pr (- pulls value))
-                            dist  (beta-distribution alpha beta)]
-                        (assoc arm :theta (draw dist))))]
-    (exploit :theta (map assoc-theta arms))))
+  (exploit :theta (map estimate-value arms)))
 
 (defn reward
   [arm reward]
--- bandit-core/test/clj_bandit/algo/bayes_test.clj
+++ bandit-core/test/clj_bandit/algo/bayes_test.clj
@@ -1,2 +1,19 @@
 (ns clj-bandit.algo.bayes-test
-  (:use [expectations]))
+  (:use [expectations]
+        [clj-bandit.algo.bayes])
+  (:require [clj-bandit.arms :as arms]))
+
+(let [a (arms/mk-arm :arm1)]
+  (given a
+         (expect :pulls 0
+                 :value 0))
+  (given (estimate-value (arms/pulled a))
+         (expect :pulls 1
+                 :value 0
+                 :alpha 1.0
+                 :beta  2.0))
+  (given (estimate-value (-> a (arms/pulled) (reward 1)))
+         (expect :pulls 1
+                 :value 1
+                 :alpha 2.0
+                 :beta  1.0)))
