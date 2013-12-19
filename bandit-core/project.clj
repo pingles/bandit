9f2143b37e2603053ce01bc7dff012de4b3c55fd
Paul Ingles <paul@oobaloo.co.uk>
2013-12-19 16:14:44 +0000

    aot compile arms ns to access Arm record

--- bandit-core/project.clj
+++ bandit-core/project.clj
@@ -7,4 +7,5 @@
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [incanter/incanter-core "1.5.1"]]
   :profiles {:dev {:dependencies [[expectations "1.4.45"]]}}
-  :plugins [[lein-expectations "0.0.8"]])
+  :plugins [[lein-expectations "0.0.8"]]
+  :aot [clj-bandit.arms])
