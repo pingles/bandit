670c88c274d59ca973d071f6ea8d4b1b08a17db9
Paul Ingles <paul@oobaloo.co.uk>
2014-03-25 14:27:42 +0000

    try removing dependencies from parent project for travis-ci build

--- project.clj
+++ project.clj
@@ -3,8 +3,5 @@
   :url "http://github.com/pingles/bandit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[bandit/bandit-core "0.2.1-SNAPSHOT"]
-                 [bandit/bandit-simulate "0.2.1-SNAPSHOT"]
-                 [bandit/bandit-ring "0.2.1-SNAPSHOT"]]
-  :plugins [[lein-sub "0.2.4"]]
+  :plugins [[lein-sub "0.3.0"]]
   :sub ["bandit-core" "bandit-simulate" "bandit-ring"])
